835b48ee52e68e70f6a794972a8c61b24f1bd69b
Rosaniline <xup6qup3@gmail.com>
2013-06-17 12:49:26 +0800

    Initial Commit

---
+++ patternBlending/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  patternBlending
+//
+//  Created by Rosani Lin on 13/6/17.
+//  Copyright (c) 2013年 Rosani Lin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
